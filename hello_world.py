95ab00af61ad7384e047f75175638dc2b94957cc
Matt Sewell <matt@pythonfinancial.com>
2018-01-24 19:26:50 +0000

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+def say_hi():
+    return "Hello World!"
+    
+    
+@app.route("/hello/<name>")
+def hello_person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten.  Awww...
+        </p>
+        <img src="http://placekitten.com/g/200/300">
+    """
+    return html.format(name.title())
+    
+    
+@app.route("/jedi/<fname>/<lname>")
+def jedi_generator(fname, lname):
+    jedi_name = lname[-3:] + fname[:2]
+    html = '''
+        <h1>
+            Hello {}!
+        </h1>
+        '''
+    return html.format(jedi_name.title())
+
+if __name__ == "__main__":
+    app.run(host=environ['IP'],
+            port=int(environ['PORT']))
+            
